7bb2249141a709109a8228d9979374e9c1e1de9b
zhangqw2 <z_q_wei@163.com>
2020-04-22 09:27:08 +0800

    add mybatis

---
+++ SpringBootMybatis/src/main/java/com/springboot/mybatis/MybatisApplication.java
@@ -0,0 +1,16 @@
+package com.springboot.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangquanwei
+ */
+@SpringBootApplication
+public class MybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+
+}
