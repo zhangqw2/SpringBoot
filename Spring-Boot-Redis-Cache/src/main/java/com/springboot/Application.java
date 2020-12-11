14fca89dd0130145727fabc7a2ea50f5a8e22af4
zhangqw2 <z_q_wei@163.com>
2020-12-11 16:44:16 +0800

    add RedisCache

---
+++ Spring-Boot-Redis-Cache/src/main/java/com/springboot/Application.java
@@ -0,0 +1,18 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+
+/**
+ * @author zhangquanwei
+ */
+@SpringBootApplication
+@EnableCaching
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
